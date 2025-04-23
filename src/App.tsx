f6d14000c1904be749dc21acd51f261b6b9ef294
AbroShahzeb <khizr772@gmail.com>
2025-04-23 23:35:18 +0500

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="text-2xl font-medium items-center justify-center flex flex-col h-screen">
+      Hello world
+    </div>
+  );
+}
+
+export default App;
